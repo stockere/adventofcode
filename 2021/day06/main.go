c13a8692134fa3d1209a0e9d312f826ae9b08982
Elizabeth Sechter <esechter@amazon.com>
2021-12-28 08:50:09 -0800

    Day 6 Part 2

--- 2021/day06/main.go
+++ 2021/day06/main.go
@@ -6,43 +6,83 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	days := 256
 	ages, err := getInput("2021/day06/input.csv")
 	if err != nil {
 		panic(err)
 	}
 	var school []*Lanternfish
 	for _, age := range ages {
-		fish := Lanternfish{age: age}
+		fish := Lanternfish{age: age, daysLeft: days}
 		school = append(school, &fish)
 	}
-	days := 80
-	for i := 1; i <= days; i++ {
-		var newFish []*Lanternfish
-		for _, fish := range school {
-			baby := fish.passDay()
-			if baby != nil {
-				newFish = append(newFish, baby)
-			}
+	runningFishTally := 0
+	// fishRecord is a lookup table of Lanternfish
+	// we've calculated the progeny outcome for
+	fishRecord := make(map[Lanternfish]int)
+	for _, fish := range school {
+		progeny, ok := fishRecord[*fish]
+		if !ok {
+			progeny = fish.getProgenyCount(fishRecord)
 		}
-		school = append(school, newFish...)
+		runningFishTally += progeny
 	}
-	fmt.Println("Number of fish after day", days, len(school))
+	fmt.Println(runningFishTally)
+	fmt.Println("Time:", time.Since(start))
 }
 
 type Lanternfish struct {
 	age int
+	daysLeft int
 }
 
-func (l *Lanternfish) passDay() *Lanternfish {
-	if l.age > 0 {
-		l.age = l.age - 1
+// advanceTimer progresses time for this fish until it
+// either produces another fish (resetting this fish's timer
+// and returning the new fish) or remaining days run out,
+// whichever is first
+func (l *Lanternfish) advanceTimer() *Lanternfish {
+	if l.daysLeft == 0 {
 		return nil
+	}
+	if l.age >= l.daysLeft{
+		l.age = l.age - l.daysLeft
+		l.daysLeft = 0
+		return nil
+	}
+	l.daysLeft = l.daysLeft - l.age - 1
+	l.age = 6
+	return &Lanternfish{
+		age:      8,
+		daysLeft: l.daysLeft,
+	}
+}
+
+// getProgenyCount advances time by the number of days this Lanternfish has left
+// and returns the total number of fish that will exist when daysLeft == 0
+// each unique Lanternfish encountered by this algorithm will be added to the
+// lookup table fishRecord for efficiency
+func (l *Lanternfish) getProgenyCount(fishRecord map[Lanternfish]int) int {
+	if l.daysLeft == 0 {
+		return 1
+	}
+	if progeny, ok := fishRecord[*l]; ok {
+		return progeny
+	}
+	// grab a copy because l.advanceTimer() mutates the fish before we store it
+	fishSnapshot := *l
+	if newFishy := l.advanceTimer(); newFishy == nil {
+		progeny := l.getProgenyCount(fishRecord)
+		fishRecord[fishSnapshot] = progeny
+		return progeny
 	} else {
-		l.age = 6
-		return &Lanternfish{age: 8}
+		progeny := l.getProgenyCount(fishRecord) + newFishy.getProgenyCount(fishRecord)
+		fishRecord[fishSnapshot] = progeny
+		return progeny
 	}
 }
 
